a8eb0dddda57454562e21dd0a814ff009ec33dc2
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2023-08-15 14:49:29 +0200

    new version

--- mttfile.cpp
+++ mttfile.cpp
@@ -35,3 +35,39 @@ std::vector < char > mttfile_load_file(const char *name, std::streampos off, std
 
 	return cont;
 }
+
+std::vector < char > mttfile_load_file(const wchar_t *wname, std::streampos off, std::streamsize lim, bool str)
+{
+	std::ifstream wfile(wname, std::ios::binary);
+
+	if (wfile.is_open() == false) throw std::runtime_error("mttfile_load_file");
+
+	std::streamsize size;
+
+	if (lim == 0)
+	{
+		wfile.seekg(0, wfile.end);
+		lim = wfile.tellg();
+		size = lim;
+
+		if (str) size++;
+	}
+	else
+	{
+		size = lim;
+
+		if (str) lim--;
+	}
+
+	wfile.seekg(off);
+
+	std::vector < char > cont(size);
+
+	wfile.read(cont.data(), lim);
+	lim = wfile.gcount();
+	wfile.close();
+
+	if (str) cont[lim] = 0;
+
+	return cont;
+}
--- mttfile.hpp
+++ mttfile.hpp
@@ -5,5 +5,6 @@
 #include <fstream>
 
 std::vector < char > mttfile_load_file(const char *name, std::streampos off, std::streamsize lim, bool str);
+std::vector < char > mttfile_load_file(const wchar_t *wname, std::streampos off, std::streamsize lim, bool str);
 
 #endif
